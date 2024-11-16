31f12d65adc2af2d06030304ee1b2b8771c63a3f
Fitsum Handiso <fhandis1@msudenver.edu>
2024-11-15 18:12:21 -0700

    Got devise installed, but will need to fix the form to resolve signup errors.

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,8 @@
 class Coach < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :teams, dependent: :destroy
     validates :first_name, presence: true
     validates :last_name, presence: true
